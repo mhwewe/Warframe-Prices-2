d5bd45cb1f80dc7f04a8a99c5f441918371d98bb
MHwewe <62072036+mhwewe@users.noreply.github.com>
2025-07-29 22:08:08 +0330

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -2,9 +2,27 @@ import sys
 from BlurWindow.blurWindow import GlobalBlur
 from PyQt5.QtGui import QColor, QIcon
 from PyQt5.QtWidgets import QMainWindow, QGridLayout, QFrame, QApplication, QGraphicsDropShadowEffect
-from PyQt5.QtCore import Qt, QSize, QThread, pyqtSignal
+from PyQt5.QtCore import Qt, QSize, QThread, pyqtSignal, QObject, QThreadPool
 from detailbox import DetailBox
 from Api_Orders import orders
+import time
+
+class Worker(QObject):
+    # print('at Worker')
+    finished = pyqtSignal()
+    result = pyqtSignal(dict)
+
+    def __init__(self, item_name, frame):
+        super().__init__()
+        self.item_name = item_name
+        self.frame = frame
+
+    def search_item(self):
+        orders_dict: dict = orders(self.item_name)
+        # print('done')
+        self.result.emit(orders_dict)
+        self.finished.emit()
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -30,7 +48,9 @@ class MainWindow(QMainWindow):
         row: int = 0
         col: int = 0
         cols = 3
-        for i in self.frame_boxes:
+        for idi, i in enumerate(self.frame_boxes):
+            self.frame_boxes[idi].search.clicked.connect(self.start_thread)
+
             if 9 <= self.box_amount <= 18:
                 cols = self.box_amount/3
             if self.box_amount > 18:
@@ -46,18 +66,37 @@ class MainWindow(QMainWindow):
                 col += 1
                 row = 0
 
-        self.frame_boxes[0].search.clicked.connect(lambda: self.search_item(self.frame_boxes[0].input.text()))
+
+        # self.frame_boxes[0].search.clicked.connect(lambda: uh.search_item(self.frame_boxes[0].input.text()))
+        self.threads = []
+
 
         main_frame = QFrame(self)
         main_frame.setLayout(main_grid)
         self.setCentralWidget(main_frame)
 
 
-    def search_item(self, item_name):
-        orders_dict: dict = orders(item_name)
+    def search_item(self, orders_dict):
+        sent = self.sender()
+        orders_dict: dict = orders_dict
         for i in range(0, self.frame_boxes[0].number_listed):
-            self.frame_boxes[0].names[i].setText(orders_dict['sell'][i]['ingame_name'])
-            self.frame_boxes[0].prices[i].setText(str(orders_dict['sell'][i]['platinum']))
+            sent.frame.names[i].setText(orders_dict['sell'][i]['ingame_name'])
+            sent.frame.prices[i].setText(str(orders_dict['sell'][i]['platinum']))
+
+    def start_thread(self):
+        self.thread = QThread()
+        frame = self.sender().parent().parent()
+        self.worker = Worker(frame.input.text(), frame)
+
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.search_item)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+
+        self.worker.result.connect(self.search_item)
+
+        self.thread.start()
 
 
 if __name__ == "__main__":
